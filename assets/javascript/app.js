59d4209bec703fb56f66c5a222cae56f38a1a18e
earyzhikova <elenaryzh55@gmail.com>
2018-02-18 17:01:12 -0500

    questions &template

---
+++ assets/javascript/app.js
@@ -0,0 +1,139 @@
+// View (html/css, updating with jQuery)
+var newImg = $("<img>")
+newImg.attr("src", )
+$("#app").append();
+$("#app").text();
+$("#app").append();
+$("#app").append();
+$("#app").append();
+$("#app").append();
+$("#app").append();
+$("#app").append();
+
+var newQuestion = $("<h2>")
+newQuestion.text(questions[0])
+$("#app").append("<h2>");
+
+// Logic that runs the game (javascript)
+//object {property: value}  
+var newQuestion = {
+	questions: 	[
+    {
+		quesion: "What state's motto is 'North to Future'?",
+		answers: ["North Dakota", "Alaska", "Montana"],
+		answerIndex: [1]
+    },
+
+    {
+		quesion: "What southeastern state boasts the cities of Frog Jump, Only, and Sweet Lips?",
+		answers: ["Georgia", "South Carolina", "Tennessee"],
+		answerIndex: [2]
+    },
+
+    {
+		quesion: 'What southern U.S. city is sometimes nicknamed "Little Cuba"?',
+		answers: ["Key West", "Miami", "Cuba City"],
+		answerIndex: [1]
+    },
+
+    {
+		quesion: "What state leads the nation in copper production?",
+		answers: ["North Dakota", "South Dakota", "Arizona"],
+		answerIndex: [2]
+    },
+
+
+    {
+		quesion: "What state's name means tribal town in the Creek Ingian language?",
+		answers: ["Alabama", "Alaska", "Arkansas"],
+		answerIndex: [0]
+    },
+
+    {
+		quesion: "What state takes its name from a Spanish word meaning snow-clad?",
+		answers: ["Texas", "Colorado", "Nevada"],
+		answerIndex: [2]
+    },
+
+    {
+		quesion: "What state shares its border with only one other state?",
+		answers: ["Maine", "Michigan","Delaware"],
+		answerIndex: [0]
+    },
+
+    {
+		quesion: "What state ranks first in state boat registrations?",
+		answers: ["Florida", "North Carolina", "Michigan"],
+		answerIndex: [2]
+    },
+
+    {
+		quesion: "What state has more ghost towns than any other state?",
+		answers: ["Oregon", "Alaska", "Wyoming"],
+		answerIndex: [0]
+    },
+
+
+   ];
+
+ get gameQuestions(index) {
+ 	return this.gameQuestions[index];
+ }
+};
+ 
+// Timer
+
+
+// lots of functions 
+function isAnwerCorrect() {
+  if (yes) {}
+}// create: start button
+//  a countdown timer (show Remaining Time)
+// four answer buttons
+//start over button
+
+
+//Get user input
+//
+// 
+
+
+// create a multiple choice arrays
+
+//get the user imput and compare it to the correct answer
+
+// identify the correct answer - 
+//use the if statement (if the answer is true, show Correct!
+// if the answer is false, show Nope!  The correct answer was:...  )
+
+// set timeout after the user answers
+
+
+// run the next question
+
+//if the time is 0 and the user didn't press the button,
+//show -- Out of Time!
+// The correct answer was...
+
+//before restarting the game, 
+//stop the timer
+//show -- all done, correct answers, incorrect answers
+
+//timer stopped until checked Start Over
+
+
+//4 screens
+
+//start
+//MC question
+//result
+//final page
+
+
+
+
+
+// js
+// MCQ
+// stored together (answers and questions and choices)
+// how can I keep them together?
